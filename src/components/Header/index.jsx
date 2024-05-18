1b2ca0279fcabcb368584cfe3fcada275e4aaca4
brunnosan <brunnosan@yahoo.com.br>
2024-05-18 16:58:03 -0300

    Criar header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,20 @@
+import styled from "styled-components";
+import { IconField } from "primereact/iconfield";
+import { InputIcon } from "primereact/inputicon";
+import { InputText } from "primereact/inputtext";
+
+const HeaderContainer = styled.header``;
+
 const Header = () => {
   return (
     <>
-      Header
+      <HeaderContainer className="w-12 flex align-items-center gap-4 px-3 md:px-8 bg-primary">
+        <h1>LOGO</h1>
+        <IconField iconPosition="right" className="flex-1 md:flex-none md:w-4">
+          <InputIcon className="pi pi-search cursor-pointer" />
+          <InputText type="text" placeholder="Buscar tarefa..." className="w-full" />
+        </IconField>
+      </HeaderContainer>
     </>
   );
 }
--- src/pages/sitePages/Home/index.jsx
+++ src/pages/sitePages/Home/index.jsx
@@ -1,7 +1,47 @@
+import styled from "styled-components";
+import { Button } from "primereact/button";
+import { Dialog } from 'primereact/dialog';
+import { InputText } from "primereact/inputtext";
+import { InputTextarea } from "primereact/inputtextarea";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+const HomeContainer = styled.section``;
+
 const Home = () => {
+
+  const [ visibleDialog, setVisibleDialog ] = useState(false);
+  const { register, handleSubmit } = useForm();
+
+  const criarTarefa = (dados) => {
+    console.log(dados);
+  }
+
   return (
     <>
-      Home
+      <HomeContainer className="px-8 py-6">
+        <h1 className="flex justify-content-between align-items-center">
+          Tarefas
+          <Button
+            icon="pi pi-plus"
+            label="Nova tarefa"
+            onClick={() => setVisibleDialog(true)}
+          />
+        </h1>
+        <Dialog header="Nova tarefa" closeOnEscape visible={visibleDialog} onHide={() => setVisibleDialog(false)}>
+          <form className="flex flex-column gap-3"
+            onSubmit={handleSubmit(criarTarefa)}
+          >
+            <InputText placeholder="Nome da tarefa"
+              {...register("nome", {require: true})}
+            />
+            <InputTextarea placeholder="Descrição da tarefa"
+              {...register("descricao", {require: true})}
+            />
+            <Button type="submit" label="Salvar" />
+          </form>
+        </Dialog>
+      </HomeContainer>
     </>
   );
 }
